9b8815488fd74863740689eace623def2c16a5e5
wangmm <893110988@qq.com>
2017-12-09 14:26:37 +0800

    配置文件

--- src/main/java/com/handle/ComponentHandle.java
+++ src/main/java/com/handle/ComponentHandle.java
@@ -1,22 +1,10 @@
 package com.handle;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
 import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
-import com.tinkerpop.blueprints.impls.orient.OrientVertex;
-import com.tinkerpop.blueprints.impls.orient.OrientVertexType;
-import org.apache.commons.lang.StringUtils;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Statement;
-import java.text.MessageFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 import static com.util.PropertiesUtil.p;
@@ -25,28 +13,59 @@ import static com.util.PropertiesUtil.p;
  * Created by Administrator on 2017\12\7 0007.
  */
 public class ComponentHandle {
-    public static void main(String[] args) {
-            List<String> n3=Arrays.asList("S PEN","OLED 屏","全视曲面屏");
-            List<String> apple=Arrays.asList("SUPER RETINA HD","3D TOUCH","APPLE PENCIL");
-            List<String> n2= Arrays.asList( "2GRAM", "AR APP", "AR CORE"/*,
-                    "ARCORE", "AR",  "GOOGLE PLAY",
-                    "TF卡", "手机卡", "SIM卡", "主卡", "4G卡", "3G卡",
-                    "SD卡", "SIM卡", "MICROSD卡", "电信卡", "联通卡",
-                    "移动卡", "屏幕", "充电线",
-                    "拍照", "摄影", "微距", "色彩", "像素", "光泽度","手机套", "真机"
-                    ,"品控","耳机","电流声","贴纸"*/);
-            OrientGraph graph=new OrientGraph( p.getProperty("orient.url"), p.getProperty("orient.username"), p.getProperty("orient.password"));
-            Iterable<Vertex> vertices=graph.getVertices("@class","MobileSubjectRelevant");
-        //Iterable<Vertex> vertices=graph.getVertices("word","NOTE8");
+    static List<String> n3 = Arrays.asList("S PEN", "OLED 屏", "全视曲面屏");
+    static List<String> apple = Arrays.asList("SUPER RETINA HD", "3D TOUCH", "APPLE PENCIL");
+    static List<String> n2 = Arrays.asList("2GRAM", "AR APP", "AR CORE",
+            "ARCORE", "AR", "GOOGLE PLAY",
+            "TF卡", "手机卡", "SIM卡", "主卡", "4G卡", "3G卡",
+            "SD卡", "MICROSD卡", "电信卡", "联通卡",
+            "移动卡", "屏幕", "充电线",
+            "拍照", "摄影", "微距", "色彩", "像素", "光泽度", "手机套", "真机"
+            , "品控", "耳机", "电流声", "贴纸", "电池");
+    public static List<String> n_one = Arrays.asList(
+            "NOTE8", "GALAXY NOTE 8", "NOTE 8", "S8+",
+            "S8", "牛8", "S8 PLUS", "NOTE7", "NOTE 7",
+            "GALAXY NOTE 7", "S7 EDGE", "S7EDGE", "S6",
+            "S6 EDGE", "NOTE", "GALAXY NOTE", "GALAXY", "牛5"
+    );
 
-        vertices.forEach(vertex -> {
-            n2.forEach(n->{
-                Vertex in=graph.getVertices("word",n).iterator().next();
-                vertex.addEdge("Component",in);
+    public static void main(String[] args) {
+        OrientGraph graph = new OrientGraph(p.getProperty("orient.url"), p.getProperty("orient.username"), p.getProperty("orient.password"));
+        insertEdgeByClass(graph, "Component", "MobileSubjectRelevant");
+
+        //removeEdgeAllByClass(graph,"Component+");
+        //graph.getVertex("").de
+        //graph.removeVertex(graph.getVertex("#36:8"));
+        //graph.removeEdge(graph.getEdge("#54:0"));
+
+    }
+
+    //手机out->电池in
+    private static void insertEdgeByClass(OrientGraph graph, String label, String className) {
+        //Iterable<Vertex> vertices = graph.getVertices("@class", className);
+        n_one.forEach(n_o -> {
+            Vertex out = graph.getVertices("word", n_o).iterator().next();
+            n3.forEach(n -> {
+                Vertex in = graph.getVertices("word", n).iterator().next();
+                out.addEdge(label, in);
             });
+
         });
-        // graph.
-       /* Iterable<Edge> edges=graph.getEdgesOfClass("Component+");*/
+
+        graph.commit();
+    }
+
+    private static void removeEdgeAllByClass(OrientGraph graph, Object label) {
+        if (label instanceof String) {
+            Iterable<Edge> edges = graph.getEdgesOfClass((String) label);
+            edges.forEach(graph::removeEdge);
+
+        } else if (label instanceof Iterable) {
+            ((Iterable) label).forEach(l -> {
+                Iterable<Edge> edges = graph.getEdgesOfClass((String) l);
+                edges.forEach(graph::removeEdge);
+            });
+        }
         graph.commit();
     }
 
